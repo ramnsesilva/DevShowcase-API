d2ad23af6718d6ef62d49e3981764c88e40aa86d
Ramnsés Silva <ramnsesmaster12@gmail.com>
2026-09-24 14:49:59 -0300

    primeiro comit

---
+++ src/main/java/com/example/devshowcaseapi/DevShowcaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.devshowcaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevShowcaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevShowcaseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devshowcaseapi/DevShowcaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devshowcaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevShowcaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
